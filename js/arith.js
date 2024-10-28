d877ec52a3633e6da6212f9a96b4b2d1e8c3678e
zt64 <31907977+zt64@users.noreply.github.com>
2024-10-28 13:15:43 -0400

    add more practice scripts

--- js/arith.js
+++ js/arith.js
@@ -1,7 +1,12 @@
 // get the element
+const spanElement1 = document.getElementById("num1");
+const spanElement2 = document.getElementById("num2");
 
 // element --- property
-var num1 = parseInt(document.getElementById("n1").textContent);
+const n1 = parseInt(spanElement1.textContent);
+const n2 = parseInt(spanElement2.textContent);
+console.log(typeof n1);
+// var num1 = parseInt(document.getElementById("n1").textContent);
 
 //process
 var c = num1 + num2;
